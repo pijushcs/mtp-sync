4a58d0eb7b2fecd12ef1af141e372e3b2a3ac0f0
Pijush Chakraborty <pijush@cse.iitb.ac.in>
2016-09-05 09:09:12 +0530

    Update profileX.cpp

--- pro-kernel-contention/profileX.cpp
+++ pro-kernel-contention/profileX.cpp
@@ -20,21 +20,28 @@ void makeT(char *cX){
 	string str;	
 	char typ[20],igns[20];
 	float t,ignf;
-	long s,e,k,core=0;
+	long s,e,k,core=0, pro_r, pro_w;
+	double pro_wavg;
 	
 	freqC_fix=2800102000*atoi(cX);
-	getline(cin,str);	
+	//getline(cin,str);	
 	getline(cin,str);
-	sscanf(str.c_str(), "[ %f] [pro mod %d] %s S%ld E%ld K%ld", &ignf, &pidX, typ, &s, &e, &k);
+	sscanf(str.c_str(), "[ %f] [promod %d] %s S%ld E%ld K%ld", &ignf, &pidX, typ, &s, &e, &k);
 
 	while(str.compare("")!=0){
 		if(!strcmp(typ,"WRCU:") || !strcmp(typ,"DRCU:")){
 			wrcu[s]+=k; mrcu[s]=max(mrcu[s],k); crcu[s]++;
 			core=max(core,s);
-		}  
+		}
+		
+		if(!strcmp(typ,"IRCU:")){
+			pro_r=s; pro_w=e;
+			pro_wavg=(double)(pro_w*100.0)/(pro_r+pro_w);
+		}   
 		
 		getline(cin,str);
-		sscanf(str.c_str(),"[ %f] [pro mod %d] %s S%ld E%ld K%ld", &ignf, &pidX, typ, &s, &e, &k);
+		sscanf(str.c_str(),"[ %f] [promod %d] %s S%ld E%ld K%ld", &ignf, &pidX, typ, &s, &e, &k);
+
 	}
 
 	// Output: [#core] [#average cycles] [#max cycles]
@@ -45,7 +52,9 @@ void makeT(char *cX){
 			rcuMax=((long double)mrcu[i]/freqC_fix)*100;
 			cout<<"C"<<i<<" A"<<rcuAvg<<" M"<<rcuMax<<endl;
 		}
+
 	}
+	cout<<"I"<<": A"<<pro_wavg<<" R"<<pro_r<<" W"<<pro_w<<endl;
 }
 
 int main(int argc, char *argv[]){
